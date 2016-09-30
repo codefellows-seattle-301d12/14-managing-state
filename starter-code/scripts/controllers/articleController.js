f4ddd589587a664725214b20445a804d40ecc730
Rae Helsel <rahelsel@me.com>
2016-09-29 18:04:33 -0700

    commented out the first comment in articleController.js

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -12,6 +12,17 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*
+  1. findWhere is a SQL call that will select all articles from the articles table with the field id
+  and the value of the id property of the params property of the ctx object, which it gets from the
+  URL placeholder, and then invokes articleData, passing in the response from the SQL call.
+      and the articleData function accepts the parameter of one article
+  2. This function takes the parameters of ctx and next, and it defines an articleData function
+  3.. Then it sets the articles property on the ctx object equal to the article that gets passed into it
+  4. Then it calls the next function, which is articleController.index, and that one checks if the articles
+  property on the ctx object has a length, if so, it calls articleView.index, IF NOT, it makes a page call
+  where the url is the root. 
+  */
   articleController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
