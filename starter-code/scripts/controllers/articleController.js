6e3c2ada11b538d4c5380f1020548c89d0816f4c
Rae Helsel <rahelsel@me.com>
2016-09-29 18:25:25 -0700

    commented out the last two

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -34,7 +34,7 @@
   // COMMENT: What does this method do?  What is it's execution path?
   /* 1. findWhere is a SQL call that will select all authors from the authors table with the field 'author'
   and the value of the author name property of the params property of the ctx object (with all + replaced with a ' '), which it gets from the URL placeholder, and then calls the function authorData (which functions as above)
-    2. authorData sets ctx.articles equal to the response from our SQL query, then calls (next), which is articleController.index, which is explained above. 
+    2. authorData sets ctx.articles equal to the response from our SQL query, then calls (next), which is articleController.index, which is explained above.
   */
   articleController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
@@ -48,6 +48,8 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*Same as above, except with category instead of author :)
+  */
   articleController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -58,6 +60,9 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*1. If Article.allArticles has any content (or a length) then ctx.articles is equal to all of our articles, and next is called.
+    2. If not (no length) then we call fetchAll which passes in the function articleData (which sets ctx.articles equal to allArticles and calls next)
+  */
   articleController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.allArticles;
